77d4db8b2b9dea8fb36bc073b8c7ff784ef03114
Sergio Fenoll <sergio@fenoll.be>
2023-04-11 14:04:26 +0200

    feat: add endpoint for shortlist for sign-flows

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { app, errorHandler } from 'mu';
-import { publicationFlowsShortlistJsonApi } from './lib/jsonapi';
+import { publicationFlowsShortlistJsonApi, signFlowsShortlistJsonApi } from './lib/jsonapi';
 
 app.get('/', function(_req, res ) {
   return res.status(200).send({ title: 'Hello from the shortlist service' });
@@ -8,6 +8,18 @@ app.get('/', function(_req, res ) {
 app.get('/publication-flows', async function(_req, res) {
   try {
     const response = await publicationFlowsShortlistJsonApi();
+    return res
+      .status(200)
+      .send(response);
+  } catch (e) {
+    console.error(e);
+    next(e);
+  }
+});
+
+app.get('/sign-flows', async function(_req, res) {
+  try {
+    const response = await signFlowsShortlistJsonApi();
     return res
       .status(200)
       .send(response);
